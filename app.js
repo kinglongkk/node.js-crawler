306ae03ad7c1297efadd991ba62c231970c2a1af
Warren Xia <kxqjs@qq.com>
2017-03-16 16:45:01 +0800

    修改 目标网址 新增 每次只执行一个异步请求资源

--- app.js
+++ app.js
@@ -3,34 +3,45 @@ var fs = require('fs');
 var request = require("request");
 var cheerio = require("cheerio");
 var mkdirp = require('mkdirp');
+var async = require('async');
 
-//目标网址
-var url = 'http://me2-sex.lofter.com/tag/美女摄影?page=';
+// 目标网址
+var url = 'http://desk.zol.com.cn/meinv/1920x1080/2.html';
 
-//本地存储目录
+// 本地存储目录
 var dir = './images';
 
-//创建目录
+// 图片链接地址
+var links = [];
+
+// 创建目录
 mkdirp(dir, function(err) {
     if(err){
         console.log(err);
     }
 });
 
-//发送请求
+// 发送请求
 request(url, function(error, response, body) {
     if(!error && response.statusCode == 200) {
         var $ = cheerio.load(body);
-        $('.img img').each(function() {
+        $('.photo-list-padding a img').each(function() {
             var src = $(this).attr('src');
-            console.log('正在下载' + src);
-            download(src, dir, Math.floor(Math.random()*100000) + src.substr(-4,4));
-            console.log('下载完成');
+            src = src.replace(/t_s208x130c5/, 't_s960x600c5');
+            links.push(src);
         });
+        // 每次只执行一个异步操作
+        async.mapSeries(links, function(item, callback) {
+            console.log(item);
+            setTimeout(function() {
+                download(item, dir, Math.floor(Math.random()*100000) + item.substr(-4,4));
+                callback(null, item);
+            }, 100);
+        }, function(err, results) {});
     }
 });
 
-//下载方法
+// 下载方法
 var download = function(url, dir, filename){
     request.head(url, function(err, res, body){
         request(url).pipe(fs.createWriteStream(dir + "/" + filename));
